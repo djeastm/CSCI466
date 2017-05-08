98eaf331729667a6db0345839372fc925f729869
David Eastman <david@davidjeastman.com>
2017-05-08 11:24:18 -0400

    Deleted extra code in RunClustering.m

--- Project-3/RunClustering.m
+++ Project-3/RunClustering.m
@@ -10,25 +10,3 @@ switch method
         [clusters, U] = RunFCM(dataset, numClusters, distMeasure, fuzzifier);
 end
 end
-
-function [clusters, U, dataset] = RunExperiment(datasetName, method, distMeasure,...
-    numClusters, fuzzifier, visualize, visible, VATAndiVAT)
-%RunExperiment Runs a clustering experiment and returns the clusters,
-% a membership matrix, and indexScores for various clustering validity
-% indices
-
-%% Load Dataset
-dataset = LoadDataSet(datasetName);
-
-%% Run Clustering Algorithm
-[clusters, U] = RunClustering(dataset, numClusters, method, distMeasure, fuzzifier);
-
-%% Visualize
-if strcmp(visualize, 'true')
-    datasetName = strrep(datasetName, '_', '-');
-    id = strcat(datasetName,'-',method,'-',distMeasure,...
-        '-',int2str(fuzzifier),'-',int2str(numClusters)); 
-    Visualize(id,dataset, clusters, U, visible, VATAndiVAT);
-end
-
-end
